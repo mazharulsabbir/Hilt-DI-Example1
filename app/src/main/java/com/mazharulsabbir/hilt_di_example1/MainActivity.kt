5f4f4ccde92abdc42546793deb94833a463a625f
mazharulsabbir <mazharul15-8950@diu.edu.bd>
2020-06-21 09:42:34 +0600

    Initial commit

---
+++ app/src/main/java/com/mazharulsabbir/hilt_di_example1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mazharulsabbir.hilt_di_example1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+// https://wpanas.github.io/programming/2019/06/20/kotlin-dagger.html
+// https://codelabs.developers.google.com/codelabs/android-dagger/#0
+// https://codelabs.developers.google.com/codelabs/android-dagger-to-hilt/#1
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
